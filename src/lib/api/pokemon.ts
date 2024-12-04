af590018405cff68ab2fa1c4f9c54cb0a00fbffe
DanielJonthn <daniel.378.dj@gmail.com>
2024-12-04 17:19:08 +0700

    Detect Pokemon ID

--- src/lib/api/pokemon.ts
+++ src/lib/api/pokemon.ts
@@ -1,10 +1,10 @@
-import type { IndexPokemon } from "$lib/types/pokemons";
+import type { IndexPokemon, Pokemon } from "$lib/types/pokemons";
 
 export async function fetchPokemon() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151/");
   const json = await response.json();
 
-  const pokemon = json.results.map((pokemon: IndexPokemon) => {
+  const pokemon: [IndexPokemon] = json.results.map((pokemon: Pokemon) => {
     const id = pokemon.url.slice(34, -1);
     return {
       id,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
   import type { PageData } from "./$types";
   import { generations } from "$lib/types/generations";
+  import type { IndexPokemon } from "$lib/types/pokemons";
+  import { page } from "$app/stores";
+  import { goto } from "$app/navigation";
 
   export let data: PageData;
+
+  let pokemonID: string = $page.url.searchParams.get("pokemonID") || "";
+  $: pokemon = data.pokemon.find((pokemon) => pokemon.id === pokemonID);
+  const detailHandler = (pokemon: IndexPokemon) => {
+    pokemonID = pokemon.id;
+    goto(`?pokemonID=${pokemonID}`);
+  };
 </script>
 
 <h1>Welcome to Pokedex</h1>
+<h1>{pokemonID}</h1>
+<h2>{pokemon?.name}</h2>
 
 <div class="container">
   <div class="grid">
@@ -14,9 +26,12 @@
     {/each}
   </div>
 
+  <!-- svelte-ignore missing-declaration -->
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
   <div class="grid">
     {#each data.pokemon as pokemon (pokemon.id)}
-      <div class="cards">
+      <div class="cards" on:click={() => detailHandler(pokemon)}>
         <div class="cards-id">
           {pokemon.id}
         </div>
